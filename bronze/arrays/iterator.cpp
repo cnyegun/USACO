067da12a0e9355aebae90a7859c30de394a0d8e3
Chi Nguyen <buichinguyen1105@gmail.com>
2025-11-29 00:04:50 +0200

    .

--- bronze/arrays/iterator.cpp
+++ bronze/arrays/iterator.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+
 using namespace std;
 
 int main() {
+	srand(time(nullptr)); // generate a random seed;
+
 	vector<int> a;
 
 	for (int i = 0; i < 100; i++) {
 		a.push_back(rand() % 100);
 	}
 
-	for (vector<int>::iterator it = a.begin(); it != a.end(); it++) {
-		cout << *it;
+	int sum = 0;
+
+	for (auto number : a) {
+		cout << number << " ";
+		sum += number;
 	}
+
+	cout << endl;
+	cout << "SUM: " << sum << endl;
 
 	return 0;
 }
