163c3d42c1c7dfad4044731efc781484391a7778
Chi Nguyen <buichinguyen1105@gmail.com>
2025-11-29 12:18:08 +0200

    ctime

---
+++ bronze/ctime/ctime.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	// Goal: print time in this format: 29/9/2025 - 12:15
+	// 								   "
+	time_t result = time(nullptr);
+
+	return 0;
+}
