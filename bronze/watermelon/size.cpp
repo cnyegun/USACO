393d04f4a79a179bac2bb326e83b8f719a09aa07
Chi Nguyen <buichinguyen1105@gmail.com>
2025-12-03 16:19:17 +0200

    watermelon

---
+++ bronze/watermelon/size.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+	std::cout << sizeof(long long) * 8 << std::endl;
+}
---
+++ bronze/watermelon/wm.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	long long x;
+	cin >> x;
+
+	cout << (x % 2 == 0 && x >= 4 ? "YES" : "NO");
+
+	return 0;
+}
